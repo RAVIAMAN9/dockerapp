77b7fe5d0285077ef4f2b97efd4c86db4f2e456e
amank <ravikumarjha25@gmail.com>
2026-02-12 17:52:47 +0530

    initial

--- src/main/java/com/example/dockerapp/Hello.java
+++ src/main/java/com/example/dockerapp/Hello.java
@@ -1,14 +1,23 @@
 package com.example.dockerapp;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Hello {
 
+    @Value("${APP_ENV:UNKNOWN}")
+    private String appEnv;
+
     @GetMapping("/hello")
     public String get() {
 
-        return "Hello from argocd";
+        return "Hello from new app";
+    }
+
+    @GetMapping("/hel")
+    public String hello() {
+        return "Hello from dockerapp - Environment: " + appEnv;
     }
 }
